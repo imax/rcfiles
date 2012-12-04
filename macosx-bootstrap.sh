fa3bbb0a8cc9ec8d674d54af762b783ff10807f3
Max Ischenko <ischenko@gmail.com>
2012-12-04 16:45:32 +0200

    ubuntu to mac os x switch

---
+++ macosx-bootstrap.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+
+# curl -O https://raw.github.com/pypa/virtualenv/master/virtualenv.py && python virtualenv.py venv
+# brew
+# ruby -e "$(curl -fsSkL raw.github.com/mxcl/homebrew/go)"
+# find "Command Line tools" for Xcode
--- ubuntu-bootstrap.sh
+++
@@ -1,6 +0,0 @@
-
-sudo apt-get install s3cmd \
-  python-setuptools python-virtualenv \
-  vim-gtk mercurial
-
-mkdir ~/projects
