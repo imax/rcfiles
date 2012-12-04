cff059c9f517d0b15f964a9d65559ee9e8bff629
Max Ischenko <ischenko@gmail.com>
2012-12-04 18:31:13 +0200

    get rid of ./bin scripts

--- install.sh
+++ install.sh
@@ -12,7 +12,7 @@ function link() {
   [ -e $dest ] || { ln -vs $here/$1 `dirname $dest` ; }
 }
 
-for name in `find . -maxdepth 1 -not -name bin -not -iname .hg \
+for name in `find . -maxdepth 1 -not -not -iname .hg \
   -not -name .ssh -not -iname install.sh -not -name . -not -name .hgignore`
 do
   link $name
@@ -20,12 +20,6 @@ done
 
 [ -d ~/.ssh ] || { mkdir ~/.ssh && chmod 700 ~/.ssh; }
 link .ssh/config
-
-[ -d ~/bin ] || { mkdir ~/bin; }
-for name in `find bin -maxdepth 1 -not -name bin`
-do
-  link $name
-done
 
 # OLD: plain tar/cp
 # tar cvf - . --exclude=.hg --exclude=install.sh  | (cd $destdir && tar xf -)
